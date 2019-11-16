f88d41c76139255ad99cefde0478e1fc9ed6f46a
Moshe Jonathan Gordon Radian <vehpus@gmail.com>
2019-11-16 10:54:04 +0200

    Inline errors for multiple lines :)

--- static/index.js
+++ static/index.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
             if (!nextInput || nextInput.length === 0 || (!!nextInput.val() && !!nextInput.val().trim())) {
                 $(e.target).closest("tr").after('<tr>\n' +
                 '                    <td class="mathInput"><input type="text" /></td>\n' +
-                '                    <td class="status" style="background-color: #aaa"></td>\n' +
+                '                    <td style="background-color: #aaa"><p class="status"></p></td>\n' +
                 '                </tr>');
                 nextInput = $(e.target).closest("tr").next().find(".mathInput>input");
             }
@@ -25,40 +25,54 @@ $(document).ready(function () {
 
             const inputs = $('#math_table input');
             const lines = Array.from($('#math_table input')).map(el => el.value);
-            const lines2 = lines.filter((entry) => entry.trim() != '');
-            console.log({inputs, lines, lines2});
-            if (lines2.length <= 1) {
+            
+            console.log({inputs, lines});
+            if (lines.length <= 1) {
                 return
             }
-            const desired_lines = lines2.slice(-2);
-            const txt = desired_lines.join('\n');
-            console.log({txt});
-            $.ajax({
-                url: '/validate',
-                type: 'post', //the method to use. GET or POST
-                dataType: 'json',
-                data: {
-                    txt: txt
-                }, //your data: {key1:value1, key2:value2}
-                success: function (data) { //handle a successfull response (200 OK)
-                    console.log(data.result);
-                    if (data.result == "A-OK") {
-                        $('#check_1').text('OK!');
-                        console.log("OK");
-                    } else {
-                        $('#check_1').text('Mistake!');
-                    }
-                    const val = $('#check_1').val();
-                    console.log(val);
-                    console.log("hey")
-
-                    //here you can do with your data whatever you want
-                },
-                error: function (jqXHR, textStauts,
-                    errorThrown) { //handle every error. e.g. 500
-                    console.log(textStatus + ': ' + errorThrown);
+            for (let i = 0; i < lines.length - 1; i += 1) {
+                const desired_lines = lines.slice(i, i + 2).filter((entry) => entry.trim() != '');
+                console.log({desired_lines});
+                if (desired_lines.length !== 2) {
+                    continue;
                 }
-            });
+                const txt = desired_lines.join('\n');
+                console.log({txt});
+                $.ajax({
+                    url: '/validate',
+                    type: 'post', //the method to use. GET or POST
+                    dataType: 'json',
+                    data: {
+                        txt: txt
+                    }, //your data: {key1:value1, key2:value2}
+                    success: function (data) { //handle a successfull response (200 OK)
+                        console.log(data.result);
+                        const row = $(`#math_table tr`)[i + 2];
+                        console.log({row: $(row)});
+                        const resCell = $(row).find("p");
+                        console.log({resCell})
+                        if (data.result == "A-OK") {
+                            console.log("OK");
+                            if (resCell) {
+                                resCell.text('OK!');
+                            }
+                        } else {
+                            if (resCell) {
+                                resCell.text('Mistake!');
+                            }
+                        }
+                        if (resCell) {
+                            const val = resCell.val();
+                            console.log(val);
+                            console.log("hey")
+                        }
+                    },
+                    error: function (jqXHR, textStauts,
+                        errorThrown) { //handle every error. e.g. 500
+                        console.log(textStatus + ': ' + errorThrown);
+                    }
+                });
+            }
             
             $(this).trigger("enterKey");
         }
