e08c36606d29cd43b9169972bb8bd577d2c37b57
Moshe Jonathan Gordon Radian <vehpus@gmail.com>
2019-11-30 18:57:51 +0200

    Added a MathLive input component for math functions

--- web-client/src/components/MathTable/index.tsx
+++ web-client/src/components/MathTable/index.tsx
@@ -1,27 +1,39 @@
 import React from "react";
+import { MathFieldComponent } from 'react-mathlive';
 
 import classNames from "./styles.module.scss";
 
 export const MathTable = () => {
+    const [mathInputs, setMathInputs] = React.useState<string[]>([""])
     return <table className={classNames.MathTable}>
         <colgroup>
             <col span={1} style={{ width: "80%" }} />
             <col span={1} style={{ width: "20%" }} />
         </colgroup>
         <thead>
-            <th>Expression</th>
-            <th>Validity</th>
+            <tr>
+                <th>Expression</th>
+                <th>Validity</th>
+            </tr>
         </thead>
         <tbody>
-            <tr>
-                <td className="mathInput">
-                    <span className="math-field"></span>
+            {mathInputs.map((latex, idx) => <tr key={idx}>
+                <td className="mathInput" title={latex}>
+                    {/* // @ts-ignore */}
+                    <MathFieldComponent
+                        latex={latex}
+                        onChange={(newLatex: string) => setMathInputs([
+                            ...mathInputs.slice(0, idx),
+                            newLatex,
+                            ...mathInputs.slice(idx + 1)
+                        ])}
+                    />
                     {/* <!-- <input type="text" /> --> */}
                 </td>
                 <td style={{ backgroundColor: "#aaa" }}>
                     <p className="status"></p>
                 </td>
-            </tr>
+            </tr>)}
         </tbody>
     </table>;
 }
