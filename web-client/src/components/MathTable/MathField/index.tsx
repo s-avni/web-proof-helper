64de065e1008a354b8250fb2f4aedce5987feeb5
Moshe Jonathan Gordon Radian <vehpus@gmail.com>
2020-02-22 13:49:51 +0200

    Input field is now positioned on top of virtual keyboard when using it to be able to focus on writing expressions

--- web-client/src/components/MathTable/MathField/index.tsx
+++ web-client/src/components/MathTable/MathField/index.tsx
@@ -2,25 +2,41 @@ import React from "react";
 import { MathFieldComponent } from 'react-mathlive';
 import { Props as MathFieldComponentProps } from 'react-mathlive/dist/MathFieldComponent'
 
+import "./styles.module.scss";
 export interface MathFieldProps {
     latexInput: string;
     getMathliveRef: MathFieldComponentProps["mathFieldRef"]
     onChange: MathFieldComponentProps["onChange"]
 }
 
+type MathLiveInput = any;
+
 export const MathField: React.FunctionComponent<MathFieldProps> = ({
     getMathliveRef,
     latexInput,
     onChange
 }) => {
-    return <MathFieldComponent
-        latex={latexInput}
-        mathFieldRef={getMathliveRef}
-        onChange={onChange}
-        // https://docs.mathlive.io/tutorial-CONFIG.html
-        mathFieldConfig={{
-            virtualKeyboardMode: "manual",
-            onBlur: (mathField: any) => mathField.$perform("hideVirtualKeyboard")
-        }}
-    />;
+    const [isVirtualKeyboardVisible, setIsVitualKeyboardVisible] = React.useState(false)
+    return <span className={isVirtualKeyboardVisible ? "withVirtualKeyboard" : undefined}>
+        <MathFieldComponent
+            latex={latexInput}
+            mathFieldRef={getMathliveRef}
+            onChange={onChange}
+            // https://docs.mathlive.io/tutorial-CONFIG.html
+            mathFieldConfig={{
+                virtualKeyboardMode: "manual",
+                onBlur: (mathField: MathLiveInput) => mathField.$perform("hideVirtualKeyboard"),
+                onVirtualKeyboardToggle: (mathField: MathLiveInput, isVisible: boolean) => {
+                    setIsVitualKeyboardVisible(isVisible);
+                    if (isVisible) {
+                        mathField.$insert(latexInput || " ", {
+                            focus: true,
+                            mode: "math",
+                            replaceAll: true
+                        });
+                    }
+                }
+            }}
+        />
+    </span>;
 }
